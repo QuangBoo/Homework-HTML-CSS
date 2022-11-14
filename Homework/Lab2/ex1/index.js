e2c492cc668000dfd6abadaac7f5a0afbec56714
QuangBoo <19522099@gm.uit.edu.vn>
2022-11-14 15:03:41 +0700

    update

---
+++ Homework/Lab2/ex1/index.js
@@ -0,0 +1,28 @@
+function goToProfile(){
+    const element1 = document.getElementById("Profile");
+    element1.scrollIntoView();
+}
+function goToObjective(){
+    const element2 = document.getElementById("Objective");
+    element2.scrollIntoView();
+}
+function goToEducation(){
+    const element3 = document.getElementById("Education");
+    element3.scrollIntoView();
+}
+function goToProject(){
+    const element = document.getElementById("skill_footer");
+    element.scrollIntoView();
+}
+function goToInterest(){
+    const element = document.getElementById("Interest");
+    element.scrollIntoView();
+}
+function goToSkill(){
+    const element = document.getElementById("Skill");
+    element.scrollIntoView();
+}
+function goToLanguages(){
+    const element = document.getElementById("Languages");
+    element.scrollIntoView();
+}
